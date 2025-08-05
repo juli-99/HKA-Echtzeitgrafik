b8e04153f82931c29b4b1fc1fee33119ac3fc77f
David <73255970+Shelicus@users.noreply.github.com>
2025-08-05 11:17:07 +0200

    cube data to data.h

--- src/Shader.hpp
+++ src/Shader.hpp
@@ -9,7 +9,7 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <shared/data.h>
+
 
 #include "helper/RootDir.h"
 
--- src/shared/data.h
+++ src/shared/data.h
@@ -7,3 +7,80 @@ typedef unsigned int GLuint;
 static const GLuint WIDTH = 1024, HEIGHT = 1024;
 static const GLuint INFOLOG_LEN = 512;
 
+float cubePhong[] = {
+    // Position           Normal
+    // Back face
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, -1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, -1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 0.0f, -1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, -1.0f,
+
+    // Front face
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+
+    // Left face
+    -0.5f,  0.5f,  0.5f,  -1.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  -1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  -1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  -1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  -1.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  -1.0f, 0.0f, 0.0f,
+
+    // Right face
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+
+     // Bottom face
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, -1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f, 0.0f,
+
+    // Top face
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+};
+
+GLfloat triangle[] =
+{
+    // Position           Color   
+     0.9f, -0.9f, 0.0f,   1.0f, 0.0f, 0.0f,
+    -0.9f, -0.9f, 0.0f,   0.0f, 1.0f, 0.0f,
+     0.0f,  0.9f, 0.0f,   0.0f, 0.0f, 1.0f
+}; //sonst mehrfach erzeugt über class
+
+float rectangle[] =
+{
+    // first triangle
+     0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // top right
+     0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // bottom right
+    -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // top left 
+    // second triangle
+     0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom left
+    -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f  // top left
+};
+
+unsigned int indices[] =
+{  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+};
+
